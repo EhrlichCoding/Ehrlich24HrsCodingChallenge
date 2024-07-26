89df8f0693cf44f842ea4cf0ea37bbd6e044d68b
Andrew Coros <andrew_d213@yahoo.com>
2024-07-27 01:36:41 +0800

    added generic repository

---
+++ sales.core/Interface/IRepository/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sales.core.Interface.IRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Add(T entity);
+        Task<IEnumerable<T>> AddRange(IEnumerable<T> items);
+        Task<T> Edit(T entity);
+        Task<T> FindById(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> Get(Expression<Func<T, bool>> predicate);
+        Task<bool> Delete(T entity);
+        Task<double> Count();
+    }
+}
---
+++ sales.infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using sales.infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sales.infrastructure.Repository
+{
+    public class GenericRepository<T> where T : class
+    {
+        private readonly SalesContext _db = new SalesContext();
+
+        public async Task<T> Add(T entity)
+        {
+            var item = _db.Set<T>().Add(entity);
+            await _db.SaveChangesAsync();
+            return item;
+        }
+        public async Task<IEnumerable<T>> AddRange(IEnumerable<T> items)
+        {
+            var item = _db.Set<T>().AddRange(items);
+            await _db.SaveChangesAsync();
+            return item;
+
+        }
+        public async Task<T> Edit(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+        public async Task<T> FindById(int id)
+        {
+            return await _db.Set<T>().FindAsync(id);
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _db.Set<T>().ToListAsync();
+        }
+        public async Task<IEnumerable<T>> Get(Expression<Func<T, bool>> predicate)
+        {
+            return await _db.Set<T>().Where(predicate).ToListAsync();
+        }
+        public async Task<bool> Delete(T entity)
+        {
+            try
+            {
+                _db.Entry(entity).State = EntityState.Deleted;
+                await _db.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public async Task<double> Count()
+        {
+            return await _db.Set<T>().CountAsync();
+        }
+    }
+}
